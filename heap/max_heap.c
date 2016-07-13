d8c2acca814dd3ecadbf6225a89fae0b4fb9531b
Peter Lesslie <plesslie@tradebot.com>
2016-07-13 07:47:01 -0500

    Simplify pop

--- heap/max_heap.c
+++ heap/max_heap.c
@@ -19,35 +19,15 @@ int32_t max_heap_push(uint32_t * restrict heap, uint32_t size) {
 }
 
 void max_heap_pop(uint32_t * restrict heap, uint32_t size) {
-    if (size > 0) {
-        heap[0] = heap[size - 1];
-        int idx = 0;
-        int c1;
-        int c2;
-        int mdx;
-        int tmp;
-
-        while (1) {
-            c1 = idx * 2 + 1;
-            c2 = c1 + 1;
-            if (c2 < size) {
-                mdx = heap[c1] > heap[c2] ? c1 : c2;
-                if (heap[idx] < heap[mdx]) {
-                    tmp = heap[mdx];
-                    heap[mdx] = heap[idx];
-                    heap[idx] = tmp;
-                    idx = mdx;
-                } else {
-                    break;
-                }
-            } else if (c1 < size && heap[idx] < heap[c1]) {
-                tmp = heap[c1];
-                heap[c1] = heap[idx];
-                heap[idx] = tmp;
-                idx = c1;
-            } else {
-                break;
-            }
-        }
+    heap[0] = heap[size - 1];
+    int k = 0;
+    int j = 1;
+    int t;
+    while (j < size) {
+        if (j < size && heap[j] < heap[j+1]) ++j;
+        if (heap[k] >= heap[j]) break;
+        t = heap[k];
+        heap[k] = heap[j];
+        heap[j] = t;
     }
 }
